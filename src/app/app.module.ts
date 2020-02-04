85ab1d5f4e226abea0ba17700268e6a7441b3cb0
Totati <attila.th@hotmail.com>
2020-02-05 00:03:40 +0100

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCheckboxModule } from '@angular/material-experimental/mdc-checkbox';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MatCheckboxModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
